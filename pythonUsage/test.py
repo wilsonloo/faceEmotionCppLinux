d9180037685490f5f4a00a39c05d5a60b8d49340
wilson <wilson@aliyun.com>
2020-09-03 19:15:44 +0800

    pythong 可以处理c++ 函数返回的结构体

--- pythonUsage/test.py
+++ pythonUsage/test.py
@@ -2,8 +2,30 @@ import sys
 sys.path.append('/usr/local/lib64')
 sys.path.append('/home/wilson/faceEmotionCppLinux/lib')
 
-import ctypes  
-fe = ctypes.cdll.LoadLibrary("libfaceEmotion.so")   
+import ctypes
+from ctypes import *
+
+# 加载内部库
+fe = cdll.LoadLibrary("libfaceEmotion.so")   
+
+# 获取版本信息
 getFaceEmotionVersion = fe.fe_getVersion
 getFaceEmotionVersion.restype = ctypes.c_char_p
 print(getFaceEmotionVersion())
+
+# 测试传递结构体
+class RecognizeResult(Structure):
+    _fields_ = [
+        ("code", ctypes.c_int),
+        ("name", ctypes.c_char * 100),
+        ("confidenceLevel", ctypes.c_float)
+    ]
+
+recognizeFunc = fe.fe_recognize
+recognizeFunc.restype = (RecognizeResult)
+ret = recognizeFunc("/dummy")
+print(ret)
+print("code:", ret.code)
+print("name:", ret.name)
+print("confidenceLevel:", ret.confidenceLevel)
+
--- source/libFaceEmotion/libFaceEmotion.cpp
+++ source/libFaceEmotion/libFaceEmotion.cpp
@@ -1,5 +1,42 @@
+#include <string.h>
+#include <stdio.h>
+    
 extern "C" 
-const char* fe_getVersion()                                                    
-{                                                                                                 
-    return "face-emotion version: 1.0.0";
+{
+    const char* fe_getVersion()                                                    
+    {                                                                                                       return "face-emotion version: 1.0.0";
+    }
+
+    struct RecognizeResult
+    {
+        // 错误码（等同于arcface的错误码）
+        int code; 
+
+        // 识别到的对方名字
+        char name[100];   
+        
+        // 识别置信等级
+        float confidenceLevel;          
+    };
+
+    // 识别图片
+    // @param imagePath 待识别的图片地址
+    // @return 识别结果，name[0] == null 表示无法识别
+    struct RecognizeResult fe_recognize(const char* imagePath)
+    {
+        printf("\nrecognizing %s...\n", imagePath);
+        
+        RecognizeResult ret;
+        ret.code = 1111;
+        
+        memset(&ret.name[0], 0, sizeof(ret.name));
+        ret.name[0] = 'a';
+        ret.name[1] = 'b';
+        ret.name[2] = 'c';
+        ret.name[3] = '\0';
+
+        ret.confidenceLevel = 345.678f;
+        
+        return ret;
+    }
 }
